f4bfa1c614c6238c690dd3670f5dc21c0d0e64c3
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2024-03-06 02:00:26 +0530

    Added error notifiying

--- models/index.js
+++ models/index.js
@@ -10,7 +10,16 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 if (process.env.NODE_ENV == "production") {
   sequelize = new Sequelize(
     "appointment_db_oryd",
@@ -19,16 +28,6 @@ if (process.env.NODE_ENV == "production") {
     config
   );
 } else {
-  if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  } else {
-    sequelize = new Sequelize(
-      config.database,
-      config.username,
-      config.password,
-      config
-    );
-  }
 }
 
 fs.readdirSync(__dirname)
